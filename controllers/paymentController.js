0af2551992ea55f896df66113c6a075fbe02a495
Arshdeep-13 <arshdeeprooprai@gmail.com>
2024-08-20 19:04:33 +0530

    change

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -2,8 +2,8 @@ const Razorpay = require("razorpay");
 const crypto = require("crypto");
 
 const instance = new Razorpay({
-  key_id: "rzp_test_6Fsll3myRMs9xe",
-  key_secret: "2ZxuxUnbMuIvBPz0avekYoh6",
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
 const createOrder = async (req, res) => {
@@ -24,19 +24,19 @@ const createOrder = async (req, res) => {
   }
 };
 
-const verifyPayment = async (req,res) => {
+const verifyPayment = async (req, res) => {
   try {
-    console.log(req.body);
+    // console.log(req.body);
     const { razorpay_payment_id, razorpay_order_id, razorpay_signature } =
       req.body;
     const body = razorpay_order_id + "|" + razorpay_payment_id;
     const expectedSignature = crypto
-      .createHmac("sha256", "2ZxuxUnbMuIvBPz0avekYoh6")
+      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
       .update(body.toString())
       .digest("hex");
     const isAuth = razorpay_signature === expectedSignature;
     if (isAuth) {
-        console.log("Payment successfull")
+      console.log("Payment successfull");
       res.redirect("http://localhost:5173/payment-successfull");
     }
   } catch (error) {
