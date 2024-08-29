568f1f066e7ea875f62d055f908026fd1d026ec4
Arshdeep-13 <arshdeeprooprai@gmail.com>
2024-08-29 23:24:50 +0530

    29-08-2024

--- config/db.js
+++ config/db.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const dbConnect = async () => {
   try {
     await mongoose.connect(process.env.DB_STRING);
-    // await mongoose.connect("mongodb+srv://devain25:123456788@data-cluster.7q1yssi.mongodb.net/?retryWrites=true&w=majority&appName=DATA-Cluster");
     console.log("Connected to db");
   } catch (e) {
     console.log("error connecting to database: ", e);
